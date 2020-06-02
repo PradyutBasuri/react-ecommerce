83768e669c86d4b2e818221049e69278d5365978
Pradyut <pradyut@unifiedinfotech.net>
2020-06-03 01:31:56 +0530

    update memu-item to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
+import "./App.css";
+import { Switch, Route,Link } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => {
+  //console.log(props1.match.params.topicId)
+  return (
+    <div>
+      <h1>Topic Detail Page</h1>
+    </div>
+  );
+};
 function App() {
-  return  <HomePage />;
-  
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import './menu-item.styles.scss';
-const MenuItem= ({title,imageUrl,size})=>(
-  <div  className={`${size} menu-item`}>
+import { withRouter } from 'react-router-dom';
+const MenuItem= ({title,imageUrl,size,history,match,linkUrl})=>(
+   
+  <div  className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
   <div className="background-image" style={{
       
       backgroundImage : `url(${imageUrl})`
@@ -14,4 +17,5 @@ const MenuItem= ({title,imageUrl,size})=>(
     </div>
 </div>
 )
-export default MenuItem;
+export default  withRouter(MenuItem);
+//export default MenuItem;
