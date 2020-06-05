0a3dce818fc1c6342b89831bb184d398175084ae
Pradyut <pradyut@unifiedinfotech.net>
2020-06-05 14:36:32 +0530

    Sign up and create data into google firebase.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SigninAndSignupPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createuserProfileDocument } from "./firebase/firebase.utils";
 class App extends Component{
 constructor(){
   super();
@@ -18,9 +18,26 @@ constructor(){
 unSubscribeFromAuth= null;
 componentDidMount(){
   //open Subscription
-  this.unSubscribeFromAuth= auth.onAuthStateChanged(user=>{
-    this.setState({currentUser:user});
-    console.log(user)
+  this.unSubscribeFromAuth= auth.onAuthStateChanged(async userAuth=>{
+  if(userAuth){
+    const userRef= await createuserProfileDocument(userAuth);
+    userRef.onSnapshot(snapShot=>{
+      this.setState({
+        currentUser:{
+          id:snapShot.id,
+          ...snapShot.data()
+        }
+      },()=>{
+        console.log(this.state);
+      });
+      console.log(this.state);
+    });
+   
+   }
+   else{
+     this.setState({currentUser:userAuth});
+   }
+  
   })
 }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,14 +12,42 @@ const firebaseConfig= {
     appId: "1:6908594432:web:6dfb5e5319b693ab6f8be5",
     measurementId: "G-ZS23SJW1CN"
 };
+
+//get call from app.js after google signin auth status changed
+export const createuserProfileDocument= async (userAuth, additionalData)=>{
+if(!userAuth) return ;
+
+//get user details from firebase database
+const userRef= firestore.doc(`users/${userAuth.uid}`);
+const snapShot=await userRef.get();
+console.log(snapShot);
+//create new user if exists false
+if(!snapShot.exists){
+    const {displayName, email}= userAuth;
+    const createdAt=new Date();
+//create new user 
+    try{
+        await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+        })
+    }
+    catch(error){
+console.log('error using user create',error.message);
+    }
+}
+return userRef;
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
+//expot firebase auth and firestore database and use in app.js
 export const auth = firebase.auth();
 export const firestore =firebase.firestore();
 
-
+//adding ggole service provider and make google sign in
 const provider=new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle= ()=>auth.signInWithPopup(provider)
