a9c50601fcecb8e52c5e51adeb58506c801cfe05
Pradyut <pradyut@unifiedinfotech.net>
2020-06-08 00:53:10 +0530

    add cart icon, cart dropdown coponents, cart reducer

---
+++ src/redux/cart/cart.action.js
@@ -0,0 +1,7 @@
+import cartActionType from './cart.types';
+
+ const toggleCartHidden =()=>({
+    type:cartActionType.TOGGLE_CART_HIDDEN,
+    
+});
+export default toggleCartHidden;
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import userReducer from '../redux/user/user.reducer';
-
+import cartReducer from '../redux/cart/cart.reducer';
 export default combineReducers({
     user:userReducer,
+    cart:cartReducer,
 });
